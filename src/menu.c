8319cd50869242bd1aff6a9ccdd5c43ba321ad32
Erik Haliewicz <ehal256@gmail.com>
2019-10-09 10:24:37 -0700

    update intro menu

--- src/menu.c
+++ src/menu.c
@@ -3,13 +3,13 @@
 #include "game.h"
 
 void print_menu() {
-    VDP_drawTextBG(PLAN_A, "BSP Renderer v0.12", 10, 8);
+    VDP_drawTextBG(PLAN_A, "BSP Renderer v0.2", 10, 8);
     VDP_drawTextBG(PLAN_A, "use d-pad to move ", 0, 12);
     VDP_drawTextBG(PLAN_A, "x/a - adjusts camera y-position", 0, 13);
     VDP_drawTextBG(PLAN_A, "y + up/dn - adjusts sector ceiling height", 0, 14);
     VDP_drawTextBG(PLAN_A, "b + up/dn - adjusts sector floor height", 0, 15);
     VDP_drawTextBG(PLAN_A, "z/c - toggles fps display / debug info", 0, 16);
-    VDP_drawTextBG(PLAN_A, "mode - toggles wireframe mode", 0, 17);
+    VDP_drawTextBG(PLAN_A, "start - toggles wireframe mode", 0, 17);
     VDP_drawTextBG(PLAN_A, "--- press any button ---", 10, 19);
 }
 
